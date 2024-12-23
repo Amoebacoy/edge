f013f668fc252da425757a39778652c8af25ad92
Amoebacoy <137673612+Amoebacoy@users.noreply.github.com>
2024-12-23 14:49:54 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '76728544-7b9a-4645-ae24-e7968a468dbb';
+let userID = 'auto';
 let proxyIP = '103.150.197.20';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
